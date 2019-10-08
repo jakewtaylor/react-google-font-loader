10a84390e1a19d23fbc953806a9ae375b71ca6ed
Jake Taylor <me@jaketaylor.co>
2019-10-08 11:26:46 +0100

    Update some dependencies

--- build/index.js
+++ build/index.js
@@ -65,60 +65,6 @@ var GoogleFontLoader = function GoogleFontLoader(_ref) {
     return null;
 };
 
-// class GoogleFontLoader extends React.PureComponent {
-//     link = null;
-
-//     createLink = () => {
-//         const { fonts, subsets } = this.props;
-
-//         const families = fonts.reduce((acc, font) => {
-//             const family = font.font.replace(/ +/g, '+');
-//             const weights = (font.weights || []).join(',');
-
-//             acc.push(family + (weights && `:${weights}`));
-
-//             return acc;
-//         }, []).join('|');
-
-//         const link = document.createElement('link');
-//         link.rel = 'stylesheet';
-//         link.href = `https://fonts.googleapis.com/css?family=${families}`;
-
-//         if (subsets && Array.isArray(subsets) && subsets.length > 0) {
-//             link.href += `&subset=${subsets.join(',')}`;
-//         }
-
-//         return link;
-//     }
-
-//     appendLink = () => document.head.appendChild(this.link);
-
-//     removeLink = () => document.head.removeChild(this.link);
-
-//     replaceLink = () => {
-//         this.removeLink();
-//         this.link = this.createLink();
-//         this.appendLink();
-//     }
-
-//     componentDidMount () {
-//         this.link = this.createLink();
-//         this.appendLink();
-//     }
-
-//     componentDidUpdate (prevProps) {
-//         if (JSON.stringify(prevProps.fonts) !== JSON.stringify(this.props.fonts)) {
-//             this.replaceLink();
-//         }
-//     }
-
-//     componentWillUnmount () {
-//         this.removeLink();
-//     }
-
-//     render = () => null;
-// }
-
 GoogleFontLoader.propTypes = {
     fonts: _propTypes2.default.arrayOf(_propTypes2.default.shape({
         font: _propTypes2.default.string.isRequired,
