39dca168d47ec03899d4a4aaa5636c1584a8673b
David Hérault <dherault@gmail.com>
2022-04-14 22:31:42 +0300

    Edit types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,17 +1,17 @@
-import * as React from 'react';
+import * as React from 'react'
 
 export interface Font {
-    font: string;
-    weights?: (string|number)[];
+    font: string
+    weights?: (string|number)[]
 }
 
 export interface GoogleFontLoaderProps {
-    fonts: Font[];
-    subsets?: string[];
-    display?: 'auto' | 'block' | 'swap' | 'fallback' | 'optional';
-    text?: string;
+    fonts: Font[]
+    subsets?: string[]
+    display?: 'auto' | 'block' | 'swap' | 'fallback' | 'optional'
+    text?: string
 }
 
-declare const GoogleFontLoader: React.FC<GoogleFontLoaderProps>;
+declare const GoogleFontLoader: React.FC<GoogleFontLoaderProps>
 
-export default GoogleFontLoader;
+export default GoogleFontLoader
